385675918f1e26b196089a95522deeaf8efdca3c
Wheels564 <53055870+Wheels564@users.noreply.github.com>
2026-02-05 15:01:35 -0500

    revert screen density commit

--- msm8937.mk
+++ msm8937.mk
@@ -20,7 +20,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Additional native libraries
 PRODUCT_COPY_FILES += \
