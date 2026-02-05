53f44ed9920118cf55b294f61b3dfa133a531a55
Wheels564 <53055870+Wheels564@users.noreply.github.com>
2026-02-05 15:21:41 -0500

    revert motoactions commit

--- msm8937.mk
+++ msm8937.mk
@@ -256,7 +256,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 # MotoActions
-ifeq ($(filter %ahannah %james %rhannah,$(TARGET_PRODUCT)),)
+ifeq ($(filter %ahannah %rhannah,$(TARGET_PRODUCT)),)
 PRODUCT_PACKAGES += \
     MotoActions
 endif
